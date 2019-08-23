b103e6a8f4d8d84cd290f9a28767f37a7b6a7e1a
Igor Fabri Cervelin <36662788+Rowzerdev@users.noreply.github.com>
2019-08-23 14:36:57 -0300

    Piscar led com potenciômetro - Source code

--- Piscar_Led_potenciometro.ino
+++ Piscar_Led_potenciometro.ino
@@ -5,12 +5,16 @@ int delayVar = 0;
 int potenciometroVar = 0;
 
 void setup() {
+  
   pinMode(13, OUTPUT);
+  
 }
 
 void loop() {
+  
   analogWrite(13, HIGH);
   delay(delayVar);
   analogWrite(13, LOW);
   delay(delayVar);
+  
 }
