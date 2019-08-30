5bb6ef411b7dbdc70306d9660c487fad97efd263
Igor Fabri Cervelin <36662788+Rowzerdev@users.noreply.github.com>
2019-08-30 10:37:38 -0300

    Arduino LED examples

---
+++ LedsRandom.ino
@@ -0,0 +1,22 @@
+int led = 0;
+
+void setup() {
+
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+
+void loop() {
+  
+  led = random(8, 13);
+  digitalWrite(led, HIGH);
+  delay(300);
+  digitalWrite(led, LOW);
+  delay(500);
+
+}
